8e5b2dc73bcf721e91dfeef002db81f8b76b6120
SVlad96 <sirbu96vlad@gmail.com>
2020-12-06 18:07:40 +0200

    Ld

---
+++ Bank1-sim/mavenproject1/src/main/java/utcluj/ro/ssatr/Bank1/sim/BankAccount.java
@@ -0,0 +1,25 @@
+package utcluj.ro.ssatr.Bank1.sim;
+
+public class BankAccount {
+
+    private String owner;
+    private int balance;
+
+    BankAccount(String owner, int balance) {
+        this.owner = owner;
+        this.balance = balance;  
+    }
+  
+    public String getOwner() { 
+        return owner;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+   
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+}
+ 
